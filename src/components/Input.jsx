bae251eadd7cb3b09c05a559fb809d0535f3e7ee
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2017-01-03 09:33:55 +0000

    :white_check_mark: added stylelint

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,9 +10,9 @@ const Input = styled.input`
     background: papayawhip;
     border: none;
     border-radius: 3px;
-    
+
     &:hover {
-        box-shadow: inset 1px 1px 2px rgba(0,0,0,0.1);
+        box-shadow: inset 1px 1px 2px rgba(0, 0, 0, 0.1);
     }
 `;
 
